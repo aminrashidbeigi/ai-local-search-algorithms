890a2bd9f38a93c6f9287e30995d05979936723b
amin.rashidbeigi <amin.rashidbeigi@zoodfood.com>
2018-07-01 18:22:25 +0430

    Efficient Keyboard Heauristic completed

--- EfficientKeyboard.py
+++ EfficientKeyboard.py
@@ -29,45 +29,38 @@ class Problem(object):
     def heuristic(self, keyboard):
         left_single_char = 0
         right_single_char = 0
-        score = 0
+        left_pair_char = 0
+        right_pair_char = 0
         single_score = 0
         pair_score = 0
-        for row in keyboard:
-            for col in row:
+        for i in range(0, 3):
+            for j in range(0, 10):
                 for key in common_chars:
-                    if col == key and row.index(col) < 5:
+                    if keyboard[i][j] == key and j < 5:
                         left_single_char = left_single_char + 1
-                    elif col == key:
+                    elif keyboard[i][j] == key:
                         right_single_char = right_single_char + 1
+                for key in common_char_pairs:
+                    key = list(key)
+                    if keyboard[i][j] == key[0]:
+                        if i > 0 and keyboard[i - 1][j] == key[1] and j < 5:
+                            left_pair_char = left_pair_char + 1
+                        elif i > 0 and keyboard[i - 1][j] == key[1] and j > 5:
+                            right_pair_char = right_pair_char + 1
+                        if i < 2 and keyboard[i + 1][j] == key[1] and j < 5:
+                            left_pair_char = left_pair_char + 1
+                        elif i < 2 and keyboard[i + 1][j] == key[1] and j > 5:
+                            right_pair_char = right_pair_char + 1
+                        if j > 0 and keyboard[i][j - 1] == key[1] and j < 5:
+                            left_pair_char = left_pair_char + 1
+                        elif j < 9 and keyboard[i][j - 1] == key[1] and j > 5:
+                            right_pair_char = right_pair_char + 1
+
         single_score = single_score + 9 - abs(right_single_char - left_single_char)
+        pair_score = pair_score + 8 - abs(right_single_char - left_single_char)
+        print(single_score + pair_score)
+        return single_score + pair_score
 
-        score = single_score + pair_score
-        return score
-
-    def successor(self, current_state):
-        all_neighbors = self.find_all_neighbors()
-        num_of_neighbors = {}
-        neighbors = []
-        for i in current_state:
-            num_of_neighbors[i] = 0
-        for neighbor in all_neighbors:
-            if current_state[neighbor[0]] == current_state[neighbor[1]]:
-                num_of_neighbors[neighbor[0]] = num_of_neighbors[neighbor[0]] + 1
-                num_of_neighbors[neighbor[1]] = num_of_neighbors[neighbor[1]] + 1
-        worst_node = max(num_of_neighbors, key=num_of_neighbors.get)
-        for i in range(1, self.number_of_colors + 1):
-            temp_state = deepcopy(current_state)
-            if temp_state[worst_node] != i:
-                temp_state[worst_node] = i
-                neighbors.append(temp_state)
-        return neighbors
-
-    def find_all_neighbors(self):
-        all_neighbors = []
-        for i in self.graph:
-            for j in graph[i]:
-                all_neighbors.append((i, j))
-        return all_neighbors
 
 
 common_chars = ['a', 'i', 'n', 'o', 's', 'h', 'r']
