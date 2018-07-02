8c756c2a4e5cd0d3ad7f4c92d11f94ceae60930d
Amin Rashidbeigi <a.rashidbeigi@gmail.com>
2018-07-03 02:12:18 +0430

    crossover and mutation added

--- Algorithms/Genetic.py
+++ Algorithms/Genetic.py
@@ -1,12 +1,32 @@
 import random
 import math
+from copy import deepcopy
 
 
 class Genetic(object):
 
-    def __init__(self, problem):
+    def __init__(self, problem, population_size):
         self.problem = problem
-        self.problem_solver(problem.initial_state())
+        self.population_size = population_size
+        self.problem_solver(population_size)
 
-    def problem_solver(self, initial_state):
-        current_state = initial_state
+    def problem_solver(self, population_size):
+        current_population = [self.problem.initial_state() for i in range(0, population_size)]
+        current_evaluation = [self.problem.heuristic(member) for member in current_population]
+        attempts = 1000
+
+        while attempts > 0:
+            sorted_population = [x for _, x in sorted(zip(current_evaluation, current_population))]
+            new_population = []
+            for i in range(0, population_size):
+                if i % 2 == 1:
+                    crossover_states = self.problem.crossover(sorted_population[i], sorted_population[i-1])
+                    new_population.append(crossover_states[0])
+                    new_population.append(crossover_states[1])
+
+            random_index_to_mutation = random.randint(0, 9)
+            new_population[random_index_to_mutation] = self.problem.mutation(new_population[random_index_to_mutation])
+            current_population = deepcopy(new_population)
+            current_evaluation = [self.problem.heuristic(member) for member in current_population]
+
+            attempts = attempts - 1
